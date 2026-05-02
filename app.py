53b25396cf60ef780a87633c5010c1efcaf3e8bb
Johel <sanabriajohe98@gmail.com>
2026-05-02 04:00:54 -0300

    Bookgs

--- app.py
+++ app.py
@@ -435,3 +435,10 @@ def logout():
 
 if __name__ == "__main__":
     app.run(debug=True)
+    # =========================
+# 🚀 RUN
+# =========================
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
